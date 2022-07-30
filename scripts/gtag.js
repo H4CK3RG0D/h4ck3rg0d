7eb7c8f22f0853643760354da2343961d402baf7
H4CK3RG0D <74117599+H4CK3RG0D@users.noreply.github.com>
2022-07-30 11:55:03 -0500

    gtag

---
+++ scripts/gtag.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CB5VM99VPW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CB5VM99VPW');
+</script>
